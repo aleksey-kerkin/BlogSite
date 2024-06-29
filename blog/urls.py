afc507e6c6e00af5c3794700c9bf7725d1472a78
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-06-29 21:31:48 +0500

    Added simple templates && simple css

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = "blog"
-
-url_patterns = [
+urlpatterns = [
     # post views
     path("", views.post_list, name="post_list"),
     path("<int:id>/", views.post_detail, name="post_detail"),
